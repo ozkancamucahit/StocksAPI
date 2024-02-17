17c94ed1fd8f8cafac7b5b46acb4509c94b8f67e
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-17 18:19:49 +0300

    Add project files.

---
+++ Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿namespace api.Models
+{
+    public sealed class Comment
+    {
+        
+        public int Id { get; private set; }
+
+        public string Title { get; private set; } = String.Empty;
+        public string Content { get; private set; } = String.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public int? StockId { get; set; }
+
+        //navigation property
+        public Stock? Stock { get; set; }
+
+    }
+}
---
+++ Models/Stock.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public sealed class Stock
+    {
+        public int Id { get; private set; }
+
+        public string Symbol { get; private set; } = String.Empty;
+        public string CompanyName { get; private set; } = String.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = String.Empty;
+
+        public long MarketCap { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; } = Enumerable.Empty<Comment>();
+
+    }
+}
